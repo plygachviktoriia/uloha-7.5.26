012b835a63d83aa71f18fe0b506c389ca0f43b7c
Plygach Viktoriia <xplyhach@stuba.sk>
2025-04-05 14:43:28 +0200

    vytvorenie výberu dĺžky a plnenie poľa pomocou rand

--- 7.5.26.c
+++ 7.5.26.c
@@ -1,3 +1,25 @@
-git add .
-git commit -m "zaciatok"
-git push
+# include <stdio.h>
+# include <time.h>
+# include <stdlib.h>
+
+
+    int main()
+	{
+    	
+    	int l = 17;
+    	float n[l];
+    	
+    	printf("Zadaj rozmier massivu od 4 po 17\n");
+    	scanf("\n%d", &l);
+    	
+    	srand(time(NULL));
+		
+		for (int i = 0; i < l; i++){
+		
+		n[i] = 4.0 + ((rand() % 1701) / 100.0);
+		printf("%.2f ", n[i]);	
+		
+		}  
+			  	
+	}
+	
