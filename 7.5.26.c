a4568d7b0c77b7fd149d0629ca9c02ddf26b92a5
Plygach Viktoriia <xplyhach@stuba.sk>
2025-04-06 14:25:57 +0200

    urobila tento algoritmus aj dla lok max

--- 7.5.26.c
+++ 7.5.26.c
@@ -2,15 +2,22 @@
 # include <time.h>
 # include <stdlib.h>
 
-    void vnutorny_rozsah(float *n, int l, float *mxlmn){
+    void vnutorny_rozsah(float *n, int l, float *mxlmn, float *mnlmx){
 	
     *mxlmn = n[0];
+    *mnlmx = n[0];
+    
 	for (int i = 1; i < l - 1; i++){
 		
 		if (n[i - 1] > n[i] && n[i] < n[i + 1] && n[i] < *mxlmn)
 		
 		{
 			 *mxlmn = n[i];
+		}
+		
+		if (n[i - 1] < n[i] && n[i] > n[i + 1] && n[i] > *mnlmx)
+		{
+			*mnlmx = n[i];
 		}
 		
 		}
@@ -23,7 +30,7 @@
     	int l = 17;
     	float n[l];
     	float mxlmn;
-    	
+    	float mnlmx;
     	
     	printf("Zadaj rozmier massivu od 4 po 17\n");
     	scanf("\n%d", &l);
@@ -40,8 +47,10 @@
 		
 		
 		}
-		vnutorny_rozsah(n, l, &mxlmn);
+		vnutorny_rozsah(n, l, &mxlmn, &mnlmx);
+		
 			printf("\nlokal min: %.2f", mxlmn);
+			printf("\nlokal max: %.2f", mnlmx);
 			
 			
 		return 0;  
