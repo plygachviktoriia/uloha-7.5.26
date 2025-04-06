0ee61669f6502ca8804e882129cb9dc9b71efbbe
Plygach Viktoriia <xplyhach@stuba.sk>
2025-04-06 12:57:39 +0200

    Zadala som funkciu v podmienenom operatore na vypocet min

--- 7.5.26.c
+++ 7.5.26.c
@@ -2,12 +2,35 @@
 # include <time.h>
 # include <stdlib.h>
 
-    
+    void vnutorny_rozsah(float *p, int l, float *mxlmn, float *mnlmx){
+	
+	float n[l];
+	
+	for (int i = 1; i < l - 1; i++){
+		
+		if (n[i - 1] > n[i] && n[i] < n[i + 1])
+		
+		{
+			printf("\nlokal min: %.2f", n[i]);
+		}
+		
+		float *mxlmn[1];
+		
+		if (n[i] < *mxlmn[1])
+		{
+		    *mxlmn[1] = n[i];
+		}
+	       
+	}	
+	}
+	
     int main()
 	{
     	
     	int l = 17;
     	float n[l];
+    	float mxlmn;
+    	
     	
     	printf("Zadaj rozmier massivu od 4 po 17\n");
     	scanf("\n%d", &l);
@@ -22,17 +45,16 @@
 		n[i] = (float)cely + (drob / 1000.0f);
 		printf("%.2f ", n[i]);	
 		
+		
 		}
 		
+			printf("\nlokal min: %.2f", mxlmn);
+			
+			
 		return 0;  
 	  	
 	}
 	
-	void vnutorny_rozsah(float *p, int l, float *mxlmn, float *mnlmx){
 	
-	if (int i = 1; i < l - 2; i++){
-	       
-	}	
-	}
 	
 	
